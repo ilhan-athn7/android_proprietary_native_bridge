2f5e0d49303afe0b15464a26630e554730b17b3a
natsumerin <64072399+natsumerinchan@users.noreply.github.com>
2022-08-03 20:04:01 +0800

    Update customize.sh
    1、Update replace list
    The libndk_translation file doesn't need to be replaced, as libndk_translation depends on the original /system/etc/binfmt_misc to take effect (the file inside will be called by /system/etc/init/ndk_translation.rc)
    
    2、Create soft links
    
    3、Update default permissions

--- customize.sh
+++ customize.sh
@@ -20,39 +20,43 @@ elif [ "$ARCH" != "x64" ] || [ "$API" -lt "31" ]; then
      fi
 fi
 
-# Replace the original libhoudini or libndk_translation files
+# Replace the original libhoudini files.
 REPLACE="
 /system/bin/arm
 /system/bin/arm64
 /system/bin/houdini
 /system/bin/houdini64
-/system/bin/ndk_translation_program_runner_binfmt_misc
-/system/bin/ndk_translation_program_runner_binfmt_misc_arm64
 /system/etc/binfmt_misc
-/system/etc/ld.config.arm.txt
-/system/etc/ld.config.arm64.txt
 /system/lib/arm
-/system/lib/libndk_translation.so
-/system/lib/libndk_translation_*.so
+/system/lib/libhoudini.so
 /system/lib64/arm64
-/system/lib64/libndk_translation.so
-/system/lib64/libndk_translation_*.so
+/system/lib64/libhoudini.so
 "
+
+mkdir -p "$MODPATH/system/bin"
+mkdir -p "$MODPATH/system/etc"
+mkdir -p "$MODPATH/system/lib"
+mkdir -p "$MODPATH/system/lib64"
+ln -sf /vendor/bin/houdini "$MODPATH/system/bin/houdini"
+ln -sf /vendor/bin/arm "$MODPATH/system/bin/arm"
+ln -sf /vendor/bin/houdini64 "$MODPATH/system/bin/houdini64"
+ln -sf /vendor/bin/arm64 "$MODPATH/system/bin/arm64"
+ln -sf /vendor/etc/binfmt_misc "$MODPATH/system/etc/binfmt_misc"
+ln -sf /vendor/lib/arm "$MODPATH/system/lib/arm"
+ln -sf /vendor/lib/libhoudini.so "$MODPATH/system/lib/libhoudini.so"
+ln -sf /vendor/lib64/arm64 "$MODPATH/system/lib64/arm64"
+ln -sf /vendor/lib64/libhoudini.so "$MODPATH/system/lib64/libhoudini.so"
 
 # Default permissions
 set_perm_recursive $MODPATH 0 0 0755 0644
-set_perm $MODPATH/system/bin/houdini 0 0 0755
-set_perm $MODPATH/system/bin/houdini64 0 0 0755
-set_perm $MODPATH/system/lib/libhoudini.so 0 0 0755
-set_perm $MODPATH/system/lib64/libhoudini.so 0 0 0755
-set_perm $MODPATH/system/vendor/bin/houdini 0 0 0755
-set_perm $MODPATH/system/vendor/bin/houdini64 0 0 0755
-set_perm $MODPATH/system/vendor/lib/libhoudini.so 0 0 0755
-set_perm $MODPATH/system/vendor/lib64/libhoudini.so 0 0 0755
-set_perm $MODPATH/system/vendor/bin/arm/linker 0 0 0755
-set_perm $MODPATH/system/vendor/bin/arm64/linker64 0 0 0755
+set_perm $MODPATH/system/vendor/bin/houdini 0 2000 0755
+set_perm $MODPATH/system/vendor/bin/houdini64 0 2000 0755
+set_perm $MODPATH/system/vendor/lib/libhoudini.so 0 0 0644
+set_perm $MODPATH/system/vendor/lib64/libhoudini.so 0 0 0644
+set_perm $MODPATH/system/vendor/bin/arm/linker 0 2000 0755
+set_perm $MODPATH/system/vendor/bin/arm64/linker64 0 2000 0755
 set_perm_recursive $MODPATH/system/vendor/etc/binfmt_misc 0 0 0755 0755
-set_perm_recursive $MODPATH/system/vendor/lib/arm 0 0 0755 0755
-set_perm_recursive $MODPATH/system/vendor/lib/arm/nb 0 0 0755 0755
-set_perm_recursive $MODPATH/system/vendor/lib64/arm64 0 0 0755 0755
-set_perm_recursive $MODPATH/system/vendor/lib64/arm64/nb 0 0 0755 0755
+set_perm_recursive $MODPATH/system/vendor/lib/arm 0 0 0755 0644
+set_perm_recursive $MODPATH/system/vendor/lib/arm/nb 0 0 0755 0644
+set_perm_recursive $MODPATH/system/vendor/lib64/arm64 0 0 0755 0644
+set_perm_recursive $MODPATH/system/vendor/lib64/arm64/nb 0 0 0755 0644
